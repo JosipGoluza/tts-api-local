b5fe3b30fbbc0a86a3128cd47dcfe86dc0250740
JosipGoluza <josip.goluza12@gmail.com>
2025-04-21 16:08:24 +0200

    Fix method call and rename variables for consistency

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ class SynthesisRequest(BaseModel):
 
 @app.get("/models")
 async def get_available_models():
-    return {"models": TTSClient.get_available_models}
+    return {"models": TTSClient.get_available_models()}
 
 @app.post("/synthesize/{model_name}")
 async def synthesize_speech(model_name: str, request: SynthesisRequest):
@@ -40,26 +40,26 @@ async def synthesize_speech(model_name: str, request: SynthesisRequest):
             text=request.text,
             output_dir=AUDIO_OUTPUT_DIR,
         )
-        return {"message": coqui_audio}
+        return {"audio_id": coqui_audio}
     except ValueError as ve:
         raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get("/stream/{hash_id}")
-async def stream_audio(hash_id: str):
+@app.get("/stream/{audio_id}")
+async def stream_audio(audio_id: str):
     try:
-        file_path = AUDIO_OUTPUT_DIR / f"{hash_id}.wav"
+        file_path = AUDIO_OUTPUT_DIR / f"{audio_id}.wav"
         print(f"Attempting to stream file: {file_path}")
 
         if not file_path.is_file():
-            raise HTTPException(status_code=404, detail=f"Audio file '{hash_id}.wav' not found.")
+            raise HTTPException(status_code=404, detail=f"Audio file '{audio_id}.wav' not found.")
 
         return FileResponse(
             path=file_path,
             media_type='audio/wav',  # Explicitly set the MIME type
-            filename=f"{hash_id}.wav"  # Suggest a filename for download
+            filename=f"{audio_id}.wav"  # Suggest a filename for download
         )
 
     except HTTPException as http_exc:
