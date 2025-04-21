acddbcff5096c54caa605bc3f29d67652f2f5611
JosipGoluza <josip.goluza12@gmail.com>
2025-04-21 11:00:10 +0200

    Rename client variables

--- app/tts_interface/tts_client.py
+++ app/tts_interface/tts_client.py
@@ -3,21 +3,21 @@ from app.tts_interface.tts_interface import TTSInterface
 
 
 class TTSClient:
-    _available_strategies = {
+    _available_models = {
         "coqui_tts": CoquiTTS,
     }
 
     def __init__(self, model_name: str):
-        if model_name not in self._available_strategies:
+        if model_name not in self._available_models:
             raise ValueError(
                 f"Unknown TTS model: '{model_name}'. "
-                f"Available models: {list(self._available_strategies.keys())}"
+                f"Available models: {list(self._available_models.keys())}"
             )
 
         print(f"\nSelected TTS Model: {model_name}")
-        strategy_class = self._available_strategies[model_name]
-        self._strategy: TTSInterface = strategy_class()
+        model_class = self._available_models[model_name]
+        self._model: TTSInterface = model_class()
 
     def generate_speech(self, text: str, output_dir: str = "./output") -> str:
-        audio_file_id = self._strategy.synthesize(text, output_dir)
+        audio_file_id = self._model.synthesize(text, output_dir)
         return audio_file_id
